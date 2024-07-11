400b21b53fe191510e48495903dac7458190ed42
GreyGreyman <greygreyman@gmail.com>
2024-07-12 01:06:08 +0300

    small event repo fixes

--- src/repositories/event.py
+++ src/repositories/event.py
@@ -2,11 +2,20 @@ import logging
 from abc import ABC, abstractmethod
 
 from aiokafka import AIOKafkaProducer
+from aiokafka.errors import KafkaError
 
 from core.config import settings
 from models.event import EventEnvelope
 
 logger = logging.getLogger(__name__)
+
+
+class EventRepoBaseError(Exception):
+    """Базовая ошибка репозитория событий"""
+
+
+class EventRepoConnectionError(Exception):
+    """Ошибка соединения репозитория событий"""
 
 
 class BaseEventRepo(ABC):
@@ -20,12 +29,10 @@ class BaseEventRepo(ABC):
 class KafkaEventRepo(BaseEventRepo):
     """Репозиторий аналитических событий использующий Kafka в качестве брокера"""
 
-    def __init__(self) -> None:
+    def __init__(self, host: str, port: int, topic: str) -> None:
         # кажется not thread safe, потому создаем каждый раз
-        self.kafka_producer = AIOKafkaProducer(
-            bootstrap_servers=f"{settings.kafka.host}:{settings.kafka.port}"
-        )
-        self.topic = settings.kafka.topic
+        self.kafka_producer = AIOKafkaProducer(bootstrap_servers=f"{host}:{port}")
+        self.topic = topic
 
     async def send_event(self, event: EventEnvelope) -> None:
         """Метод для отправки события в брокер/хранилище"""
@@ -35,11 +42,14 @@ class KafkaEventRepo(BaseEventRepo):
             await self.kafka_producer.send_and_wait(
                 topic=self.topic, value=message.encode()
             )
-        except Exception:
+        except KafkaError as e:
             logger.exception(f"error while sending message to kafka {message=}")
+            raise EventRepoConnectionError from e
         finally:
             await self.kafka_producer.stop()
 
 
 def get_kafka_event_repo() -> KafkaEventRepo:
-    return KafkaEventRepo()
+    return KafkaEventRepo(
+        host=settings.kafka.host, port=settings.kafka.port, topic=settings.kafka.topic
+    )
